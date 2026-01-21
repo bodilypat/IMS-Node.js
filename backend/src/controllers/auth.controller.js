08916b76d2512bb5e4648387b055120d646a3652
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-01-21 15:16:53 -0800

    Add files via upload

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,1 +1,29 @@
-auth
+//src/controllers/auth.controller.js 
+
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { name, emai, password } = req.body;
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ name, email, password: hashed });
+    res.status(201).json({ message: 'User registered successfully', userId: user._id });
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+
+        const token = jwt.sign(
+            { i: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' } 
+        );
+        res.json({ token, role: user.role });
+    }
+};
