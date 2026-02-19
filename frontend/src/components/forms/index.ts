565642d8faac40f350f91c3a95c00820a8e2442c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-19 07:55:20 -0800

    Add files via upload

---
+++ frontend/src/components/forms/index.ts
@@ -0,0 +1,13 @@
+//src/components/forms/index.ts 
+
+export * from './base/Formwarrpper';
+export * from './base/FormInput';
+export * from './base/FormSelect';
+export * from './base/FormTextArea';
+export * from './base/FormCheckbox';
+export * from './base/FormAction';
+
+export * from './product/ProductForm';
+export * from './supplier/SupplierForm';
+export * from './order/OrderForm';
+
